02eb273bbcb2071f93be7b15b6a7e361b0e52564
alonh <alon.heller@alcatel-lucent.com>
2013-09-12 14:16:37 +0200

    added option to get localization with _

--- src/localize.js
+++ src/localize.js
@@ -82,6 +82,30 @@ angular.module('localization', [])
                 }
                 // return the value to the call
                 return result;
+            },
+	    evaluateString: function (value) {
+                // default the result to an empty string
+                var result = '';
+ 
+                // make sure the dictionary has valid data
+                if ((localize.dictionary !== []) && (localize.dictionary.length > 0)) {
+                    // use the filter service to only return those entries which match the value
+                    // and only take the first result
+                    var entry = $filter('filter')(localize.dictionary, function (element) {
+                            return element.key === value;
+                        }
+                    )[0];
+ 
+                    // set the result
+                    if (entry == null) {
+                        result = value;
+                    }
+                    else {
+                        result = entry.value;
+                    }
+                }
+                // return the value to the call
+                return result;
             }
         };
 
